5bb7c2e5f793df1597a0f176c428a5d7394c7f8d
xyzuniverse <dev.xyzuniverse@gmail.com>
2024-10-24 01:21:08 +0800

    Initial commit
    Change-Id: I2d4f4a8e203e41b6b40fcb9adf2e6d15bdc97bdb
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

---
+++ README.md
@@ -0,0 +1,20 @@
+# Keybox overlay generator
+A overlay generator for those who are lazy (including me xD)
+
+## Installation
+
+Install this project with npm
+
+```bash
+  cd <dir/clonedfolder>
+  npm install
+```
+
+## How to use
+- Copy your `keybox.xml` into current project folder
+- Run the script:
+```bash
+node index.js
+```
+
+The result is writed on `output.txt`, just copy and paste it into your CertifiedPropsOverlay.
---
+++ index.js
@@ -0,0 +1,74 @@
+const fs = require("fs");
+const xml2js = require("xml2js");
+
+const keyboxFile = fs.readFileSync("./keybox.xml");
+
+// Parse the XML
+xml2js.parseString(keyboxFile, (err, result) => {
+  if (err) {
+    throw err;
+  }
+
+  // Navigate through the parsed object to extract certificates
+  const keyboxes = result.AndroidAttestation.Keybox;
+  keyboxes.forEach((keybox) => {
+    const keys = keybox.Key;
+    const ec_keys = keys[0];
+    const rsa_keys = keys[1];
+
+    const ecPrivateKey = certTrimmer(ec_keys.PrivateKey[0]._);
+    const rsaPrivateKey = certTrimmer(rsa_keys.PrivateKey[0]._);
+    const ecCerts = ec_keys.CertificateChain[0].Certificate;
+    const rsaCerts = rsa_keys.CertificateChain[0].Certificate;
+
+    const ecCertsOut = [];
+    const rsaCertsOut = [];
+
+    // Push the keys
+    for (let x = 0; x < ecCerts.length; x++) {
+      ecCertsOut.push(
+        "    <item>EC.CERT_" +
+          (x + 1) +
+          ":" +
+          certTrimmer(ecCerts[x]._) +
+          "</item>"
+      );
+    }
+
+    for (let x = 0; x < rsaCerts.length; x++) {
+      rsaCertsOut.push(
+        "    <item>RSA.CERT_" +
+          (x + 1) +
+          ":" +
+          certTrimmer(rsaCerts[x]._) +
+          "</item>"
+      );
+    }
+
+    let result =
+      `<string-array name="config_certifiedKeybox" translatable="false">\n` +
+      `    <item>EC.PRIV:${ecPrivateKey}</item>\n` +
+      `${ecCertsOut.join("\n")}\n` +
+      `    <item>RSA.PRIV:${rsaPrivateKey}</item>\n` +
+      `${rsaCertsOut.join("\n")}` +
+      `\n</string-array>`;
+
+    fs.writeFileSync("./output.txt", result);
+    console.log("Overlay generated!");
+  });
+});
+
+function certTrimmer(array) {
+  return array
+    .split(/\r?\n|\r/g)
+    .filter(
+      (line) =>
+        !line.includes("-----BEGIN EC PRIVATE KEY-----") &&
+        !line.includes("-----END EC PRIVATE KEY-----") &&
+        !line.includes("-----BEGIN RSA PRIVATE KEY-----") &&
+        !line.includes("-----END RSA PRIVATE KEY-----") &&
+        !line.includes("-----BEGIN CERTIFICATE-----") &&
+        !line.includes("-----END CERTIFICATE-----")
+    )
+    .join("");
+}
